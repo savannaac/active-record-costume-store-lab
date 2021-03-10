a95194def561d34edae093dfaa35c610712ce954
savanna chiang <savannaac@gmail.com>
2021-03-09 23:15:26 -0600

    activerecord costume store lab

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -4,6 +4,18 @@
 # file name after the number, so in this case:
 # 002_create_costumes.rb becomes class CreateCostumes
 
+class CreateCostumes < ActiveRecord::Migration[4.2]
+    def change
+        create_table :costumes do |t|
+            t.string :name
+            t.string :price
+            t.string :size
+            t.string :image_url
+            t.timestamps
+        end
+    end
+end
+
 # Define a change method in which to do the migration
 # In this change method, create columns with the correct names and 
 # value types according to the spec
